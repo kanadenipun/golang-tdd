6fcd06fe873e93cda927198a54f38bb2118d87d3
kanadenipun <kanadenipun@gmail.com>
2023-04-09 19:47:56 +0530

    Refactor : use add from integers package

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,11 @@
 package arrays
 
+import "example/hello/integers"
+
 func Sum(sumArray [5]int) int {
 	var sumResult int
 	for _, x := range sumArray {
-		sumResult += x
+		sumResult = integers.Add(sumResult, x)
 	}
 	return sumResult
 }
