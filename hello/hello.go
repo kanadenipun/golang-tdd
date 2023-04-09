b4e76e0fb9f7dbbcf00d7083b3f0030db2adfc12
kanadenipun <kanadenipun@gmail.com>
2023-04-09 09:03:49 +0530

    Refactor : split hello into smaller functions

--- hello/hello.go
+++ hello/hello.go
@@ -2,24 +2,33 @@ package main
 
 import "fmt"
 
-func Hello(name string, lang string) string {
-	greetingStringEnglish := "Hello, "
-	greetingStringSpanish := "Hola, "
-	greetingStringFrench := "Bonjour, "
-	var greetingText string
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	greetingStringEnglish = "Hello, "
+	greetingStringSpanish = "Hola, "
+	greetingStringFrench  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name + "!"
+}
 
-	switch lang {
-	case "Spanish":
-		greetingText = greetingStringSpanish + name + "!"
-	case "French":
-		greetingText = greetingStringFrench + name + "!"
+func greetingPrefix(language string) (greetingText string) {
+	switch language {
+	case spanish:
+		greetingText = greetingStringSpanish
+	case french:
+		greetingText = greetingStringFrench
 	default:
-		greetingText = greetingStringEnglish + name + "!"
+		greetingText = greetingStringEnglish
 	}
-	return greetingText
+	return
 }
 
 func main() {
