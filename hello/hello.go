a5acb09f4de9db1283df34958154c7beba033fbb
kanadenipun <kanadenipun@gmail.com>
2023-04-09 08:33:09 +0530

    refactor, remove unnecessary var

--- hello/hello.go
+++ hello/hello.go
@@ -4,9 +4,8 @@ import "fmt"
 
 func Hello(name string) string {
 	greetingString := "Hello, "
-	defaultGreetingString := greetingString + "World!"
-	if len(name) < 1 {
-		return defaultGreetingString
+	if name == "" {
+		name = "World"
 	}
 	return greetingString + name + "!"
 }
