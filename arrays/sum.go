c0e6b105f3674cc84bb4652ebc81e6bef67e5f2e
kanadenipun <kanadenipun@gmail.com>
2023-04-09 19:43:46 +0530

    Refactor: sum of array with for range

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,8 +2,8 @@ package arrays
 
 func Sum(sumArray [5]int) int {
 	var sumResult int
-	for i := 0; i < len(sumArray); i++ {
-		sumResult += sumArray[i]
+	for _, x := range sumArray {
+		sumResult += x
 	}
 	return sumResult
 }
