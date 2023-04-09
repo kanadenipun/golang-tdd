2b1b6a1412721041fe0cfc227eaf2765c174d7d8
kanadenipun <kanadenipun@gmail.com>
2023-04-09 08:30:43 +0530

    greet hello world when no name is provided as input

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func Hello(name string) string {
 	greetingString := "Hello, "
+	defaultGreetingString := greetingString + "World!"
+	if len(name) < 1 {
+		return defaultGreetingString
+	}
 	return greetingString + name + "!"
 }
 
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Nipun")
-	want := "Hello, Nipun!"
+	t.Run("Say Hello when a name is passed", func(t *testing.T) {
+		got := Hello("Nipun")
+		want := "Hello, Nipun!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Say Hello World when name is not passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
