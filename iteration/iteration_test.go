252cf523f6745ae2f8853156fd1ff10492ba20b4
kanadenipun <kanadenipun@gmail.com>
2023-04-09 17:53:09 +0530

    Refactor test

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,9 +3,11 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-	if repeated != expected {
-		t.Errorf("expected '%v', got '%v'", expected, repeated)
-	}
+	t.Run("should repeat a five times", func(t *testing.T) {
+		repeated := Repeat("a")
+		expected := "aaaaa"
+		if repeated != expected {
+			t.Errorf("expected '%v', got '%v'", expected, repeated)
+		}
+	})
 }
